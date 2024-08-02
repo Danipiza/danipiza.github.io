babca658fc638126bec086483bbf3b2b4244ab69
DannyP39 <danipiza@ucm.es>
2024-08-02 21:23:23 +0200

    actions updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
-/*const LIVE_URL = "https://danipiza.github.io/";
+const SERVER_PORT = 3000;
+const LOCAL_HOST_URL = `http://localhost:${SERVER_PORT}/`
+
+const LIVE_URL = "https://danipiza.github.io/";
 
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
@@ -11,11 +14,12 @@ let BASE_URL = LOCAL_HOST_URL;
 
 if(isBuild){
 	BASE_URL = LIVE_URL;
-}*/
+}
 
-//site: BASE_URL,
+// site: 'https://danipiza.github.io/',
 // https://astro.build/config
 export default defineConfig({  
-  site: 'https://danipiza.github.io/',
+  server: { port: SERVER_PORT },
+  site: BASE_URL,
   integrations: [tailwind()]
 });
